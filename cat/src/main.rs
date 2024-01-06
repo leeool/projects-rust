659ac38c09a7e7f16f70abba40b2b972cb09ecae
leeool <lgonsalez02@gmail.com>
2024-01-06 16:49:59 -0300

    feat: calc elapsed time

--- cat/src/main.rs
+++ cat/src/main.rs
@@ -1,10 +1,17 @@
 use cat::run;
+use std::time::Instant;
 
 fn main() {
+    let started_at = Instant::now();
     run();
-
+    elapsed_time(started_at);
 }
 
+fn elapsed_time(started: Instant) {
+    let elapsed = Instant::now().duration_since(started);
+
+    print!("seconds: {:0.9}", elapsed.as_secs_f64());
+}
 
 
 
