3f9b6c9161ff1a4335bf7f81e7de61c820d2f34d
leeool <lgonsalez02@gmail.com>
2024-02-28 10:33:52 -0300

    Fix main error handling

--- rm/src/main.rs
+++ rm/src/main.rs
@@ -1,11 +1,11 @@
 use std::env::args;
 use rm::Config;
 
-fn main() -> Result<(), ()> {
+fn main() -> Result<(), String> {
     let args = args().skip(1).collect::<Vec<String>>();
     let config = match Config::new(args) {
         Ok(config) => config,
-        Err(e) => return Err(println!("{}", e)) 
+        Err(e) => return Err(e) 
     };
 
     println!("{:?}", config);
